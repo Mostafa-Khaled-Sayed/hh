90f903105e9b63c38d890d922c053ddafeaa4048
mostafa12388888 <kostafa606888@gmail.com>
2024-07-31 07:35:31 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,73 +1,3 @@
-// import { createApp } from "vue";
-// import App from "./App.vue";
-// import router from "./router";
-// import store from "./store";
-// import { createPinia } from "pinia";
-// import { createVuetify } from "vuetify";
-// import * as components from "vuetify/components";
-// import * as directives from "vuetify/directives";
-// import "vuetify/styles";
-// import "@mdi/font/css/materialdesignicons.css";
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-// import { fas } from "@fortawesome/free-solid-svg-icons";
-// import Toast from "vue-toastification";
-// import "vue-toastification/dist/index.css";
-// import VTooltip from "v-tooltip";
-// import mitt from "mitt";
-// import Cookies from "vue-cookies"; // Import Cookies
-// import axios from "axios";
-
-// // Initialize Pinia
-// const pinia = createPinia();
-
-// // Add FontAwesome icons
-// library.add(faCoffee, fas);
-
-// // Create Vuetify instance
-// const vuetify = createVuetify({
-//     components,
-//     directives,
-// });
-
-// // Create an event emitter
-// const Emitter = mitt();
-
-// // Create Vue app
-// const app = createApp(App);
-
-// // Configure the app with plugins and provide necessary instances
-// app.use(pinia)
-//     .use(store)
-//     .use(vuetify)
-//     .use(Cookies) // Add Cookies plugin
-//     .use(router)
-//     .use(VTooltip)
-//     .use(Toast, { position: "top-right", timeout: 3000 })
-//     .provide("Emitter", Emitter)
-//     .component("font-awesome-icon", FontAwesomeIcon);
-
-// // Detect if the app is running as PWA
-// if (window.matchMedia("(display-mode: standalone)").matches) {
-//     document.body.classList.add("pwa");
-// } else {
-//     document.body.classList.add("not-pwa");
-// }
-// // app.mount("#app");
-
-// // start backend
-// app.config.productionTip = false;
-
-// const backendUrl = process.env.VUE_APP_BACKEND_URL;
-
-// axios.defaults.baseURL = backendUrl;
-
-// app({
-//     render: (h) => h(App),
-// }).$mount("#app");
-// // end backend
-
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -86,7 +16,7 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import VTooltip from "v-tooltip";
 import mitt from "mitt";
-import Cookies from "vue-cookies";
+import Cookies from "vue-cookies"; // Import Cookies
 import axios from "axios";
 
 // Initialize Pinia
@@ -111,7 +41,7 @@ const app = createApp(App);
 app.use(pinia)
     .use(store)
     .use(vuetify)
-    .use(Cookies)
+    .use(Cookies) // Add Cookies plugin
     .use(router)
     .use(VTooltip)
     .use(Toast, { position: "top-right", timeout: 3000 })
@@ -124,10 +54,16 @@ if (window.matchMedia("(display-mode: standalone)").matches) {
 } else {
     document.body.classList.add("not-pwa");
 }
+// app.mount("#app");
 
-// Set up axios default base URL
+// start backend
+createApp.config.productionTip = false;
+
 const backendUrl = process.env.VUE_APP_BACKEND_URL;
+
 axios.defaults.baseURL = backendUrl;
 
-// Mount the Vue app
-app.mount("#app");
+new createApp({
+    render: (h) => h(App),
+}).$mount("#app");
+// end backend
